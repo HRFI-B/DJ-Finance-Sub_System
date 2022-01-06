f7e09ffc0b0287975badfbcc01264103abb67f97
HRFI-B <93549337+HRFI-B@users.noreply.github.com>
2022-01-06 18:36:10 +0700

    Added Lowercase flexibilty

--- Sistem Keuangan Daarul Jannah - Access Ver/konversi_bulan.py
+++ Sistem Keuangan Daarul Jannah - Access Ver/konversi_bulan.py
@@ -1,26 +1,26 @@
 def convert_bln(bulan_str):
     for i in range (len(bulan_str)):
-        if bulan_str == 'JANUARI':
+        if bulan_str == 'JANUARI' or 'Januari':
             return 1
-        elif bulan_str == 'FEBRUARI':
+        elif bulan_str == 'FEBRUARI' or 'Februari':
             return 2
-        elif bulan_str == 'MARET':
+        elif bulan_str == 'MARET' or 'Maret':
             return 3
-        elif bulan_str == 'APRIL':
+        elif bulan_str == 'APRIL' or 'April':
             return 4
-        elif bulan_str == 'MEI':
+        elif bulan_str == 'MEI' or 'Mei':
             return 5
-        elif bulan_str == 'JUNI':
+        elif bulan_str == 'JUNI' or 'Juni':
             return 6
-        elif bulan_str == 'JULI':
+        elif bulan_str == 'JULI' or 'Juli':
             return 7
-        elif bulan_str == 'AGUSTUS':
+        elif bulan_str == 'AGUSTUS' or 'Agustus':
             return 8
-        elif bulan_str == 'SEPTEMBER':
+        elif bulan_str == 'SEPTEMBER' or 'September':
             return 9
-        elif bulan_str == 'OKTOBER':
+        elif bulan_str == 'OKTOBER' or 'Oktober':
             return 10
-        elif bulan_str == 'NOVEMBER':
+        elif bulan_str == 'NOVEMBER' or 'November':
             return 11
-        elif bulan_str == 'DESEMBER':
+        elif bulan_str == 'DESEMBER' or 'Desember':
             return 12    
